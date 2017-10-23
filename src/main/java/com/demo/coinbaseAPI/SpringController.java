de16625522fd99c60511e52ca351e0a433845507
Ivan Wong <wongi@bu.edu>
2017-10-23 01:32:43 -0400

    removed unused imports / vars

--- src/main/java/com/demo/coinbaseAPI/SpringController.java
+++ src/main/java/com/demo/coinbaseAPI/SpringController.java
@@ -5,13 +5,10 @@ import java.io.IOException;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SpringController {
